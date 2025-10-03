78607c5efce9c1501a451759626bc944c72ace98
mhzen <88493761+mhzen@users.noreply.github.com>
2025-10-03 19:53:49 +0700

    update

--- exp/w5/numbah.cpp
+++ exp/w5/numbah.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
-    int k[10] = {0};
+    int k[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     for (int i=0; i<n; i++) {
         int m;
         cin>>m;
